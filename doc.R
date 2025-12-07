97ef79d8f099a966d10d6534cc73ac77aa049f3c
Astittva Mandloi <am24apa@herts.ac.uk>
2025-12-07 21:54:15 +0000

    initial R script with data loading and summary

--- doc.R
+++ doc.R
@@ -1,1 +1,12 @@
 ds <- read.csv("Impact_of_Remote_Work_on_Mental_Health.csv")
+
+str(ds) # sample structure
+
+
+head(ds) # Display first fives rows
+
+
+colnames(ds) # Show column names
+
+
+summary(ds) # Basic summary numeric variables
