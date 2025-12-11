0fdba70abc5ea68f00fdafac6acaa15a80931d04
Astittva Mandloi <am24apa@herts.ac.uk>
2025-12-11 16:37:22 +0000

    Select variables and recode stress to numeric

--- doc.R
+++ doc.R
@@ -16,3 +16,27 @@ data <- read.csv("Impact_of_Remote_Work_on_Mental_Health.csv",
 str(data)
 table(data$Work_Location)
 table(data$Stress_Level)
+
+data_small <- data %>%
+  select(Work_Location, Stress_Level)
+
+dim(data_small)
+colSums(is.na(data_small))
+
+data_small$Work_Location <- factor(
+  data_small$Work_Location,
+  levels = c("Hybrid", "Onsite", "Remote")
+)
+
+data_small$Stress_Level <- factor(
+  data_small$Stress_Level,
+  levels = c("Low", "Medium", "High"),
+  ordered = TRUE
+)
+
+data_small$Stress_Score <- as.numeric(data_small$Stress_Level)
+
+summary(data_small$Work_Location)
+summary(data_small$Stress_Level)
+summary(data_small$Stress_Score)
+
