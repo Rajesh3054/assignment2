1de94232b07cba546c12c79462c06602ab432f55
Astittva Mandloi <am24apa@herts.ac.uk>
2025-12-11 16:33:22 +0000

    Add libraries and basic data loading

--- doc.R
+++ doc.R
@@ -1,1 +1,18 @@
-ds <- read.csv("Impact_of_Remote_Work_on_Mental_Health.csv")
+# 7COM1079 – Group A120
+# Dataset: Impact_of_Remote_Work_on_Mental_Health.csv
+# RQ (Path 1):
+#   Does work-mode (Hybrid, Onsite, Remote) significantly
+#   affect employee stress level?
+
+# install.packages("tidyverse") ...
+library(tidyverse)
+library(car)
+library(effectsize)
+library(rstatix)
+
+data <- read.csv("Impact_of_Remote_Work_on_Mental_Health.csv",
+                 stringsAsFactors = FALSE)
+
+str(data)
+table(data$Work_Location)
+table(data$Stress_Level)
